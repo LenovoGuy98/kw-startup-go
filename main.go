8d8eeb8f779130d46bc7b276aba45ab5a01b653e
shaun <lenovoguy98@gmail.com>
2025-08-15 23:22:52 -0400

    Updated code added hostinfo

--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"net/url"
 	"os"
 	"os/user"
 	"runtime"
-	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -29,19 +27,6 @@ func main() {
 	link := widget.NewHyperlink("https://dokindworks.org", parseURL("https://dokindworks.org"))
 	println("Welcome to your new Kindworks laptop")
 
-	// User registration
-	//	nameEntry := widget.NewEntry()
-	//	nameEntry.SetPlaceHolder("Enter your name")
-	//	accountLabel := widget.NewLabel("")
-	//	registerButton := widget.NewButton("Create Account", func() {
-	//		username := nameEntry.Text
-	//		password := generatePassword(12)
-	//		accountLabel.SetText(fmt.Sprintf("Username: %s\nPassword: %s", username, password))
-	//	})
-
-	// System Information
-	//	sysInfoLabel := widget.NewLabel(getSystemInfo())
-
 	// Update Software
 	//	updateOutputLabel := widget.NewLabel("")
 	//	updateButton := widget.NewButton("Update Software", func() {
@@ -59,6 +44,17 @@ func main() {
 	//		}
 	//		updateOutputLabel.SetText(string(output))
 	//	})
+
+	// Learn about host
+
+	learnButton2 := widget.NewButton("Learn about the Host ", func() {
+		learnWindow2 := myApp.NewWindow("Learn the Host")
+		// learnText2 := widget.NewLabel("ZorinOS is Zorina user-friendly Linux distribution based on Ubuntu.\n\nHere are some basics:\n- The panel at the bottom is similar to Windows and macOS.\n- The 'Start' menu gives you access to all your applications.\n- You can install new software from the 'Software' application.")
+		learnText2 := widget.NewLabel(getSystemInfo())
+		learnWindow2.SetContent(container.NewVBox(learnText2))
+		learnWindow2.Resize(fyne.NewSize(400, 100))
+		learnWindow2.Show()
+	})
 
 	// Learn ZorinOS
 	learnButton := widget.NewButton("Learn about your Laptop", func() {
@@ -85,6 +81,7 @@ func main() {
 		//	updateButton,
 		//		updateOutputLabel,
 		learnButton,
+		learnButton2,
 	)
 
 	myWindow.SetContent(content)
@@ -109,15 +106,6 @@ func getSystemInfo() string {
 	if err != nil {
 		return "Could not get hostname"
 	}
-	return fmt.Sprintf("OS: %s\nArchitecture: %s\nUsername: %s\nHostname: %s", runtime.GOOS, runtime.GOARCH, currentUser.Username, hostname)
-}
 
-func generatePassword(length int) string {
-	rand.Seed(time.Now().UnixNano())
-	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")
-	b := make([]rune, length)
-	for i := range b {
-		b[i] = chars[rand.Intn(len(chars))]
-	}
-	return string(b)
+	return fmt.Sprintf("OS: %s\nArchitecture: %s\nUsername: %s\nHostname: %s", runtime.GOOS, runtime.GOARCH, currentUser.Username, hostname)
 }
